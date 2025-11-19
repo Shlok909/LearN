f0e7671bf023e853b67cf1613ee94e2a400292da
Shlok Sane <shloksane11@gmail.com>
2025-11-19 06:13:39 +0000

    make it's color bright blue (_for element <Button>_)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -144,7 +144,7 @@ export default function SignupPage() {
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="w-full" disabled={isLoading}>
+              <Button type="submit" className="w-full" disabled={isLoading} variant="secondary">
                 {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
                 Create Account
               </Button>
