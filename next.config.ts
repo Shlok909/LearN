6e28045c74da4f308d13fdefb2286b62c02ded14
Shlok Sane <shloksane11@gmail.com>
2025-11-19 17:31:40 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'wallpapers.com',
         port: '',
         pathname: '/**',
