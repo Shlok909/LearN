64d84b035adab83330d41a66fd324bf4ac762acd
shlok11 <shloksane11@gmail.com>
2025-11-22 11:00:29 +0530

    Create VerifyEmail.tsx with a starter React component

---
+++ src/app/verify-email/VerifyEmail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VerifyEmail: React.FC = () => {
+    return (
+        <div>
+            <h1>Verify Your Email</h1>
+            <p>Please check your inbox and click on the verification link.</p>
+        </div>
+    );
+};
+
+export default VerifyEmail;
