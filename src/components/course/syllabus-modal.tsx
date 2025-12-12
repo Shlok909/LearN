71aa7161bf595cf6b931b632697426b518faf849
Shlok Sane <shloksane11@gmail.com>
2025-12-12 18:48:54 +0000

    remove this just keep the pdf and the cross option to close the pop when

--- src/components/course/syllabus-modal.tsx
+++ src/components/course/syllabus-modal.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 
 interface SyllabusModalProps {
   isOpen: boolean;
@@ -12,14 +12,8 @@ interface SyllabusModalProps {
 export default function SyllabusModal({ isOpen, onClose, pdfUrl }: SyllabusModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="h-full w-full max-w-full flex-col p-0 sm:h-[90vh] sm:max-w-3xl sm:p-0">
-        <DialogHeader className="p-4 border-b">
-          <DialogTitle>Course Syllabus</DialogTitle>
-          <DialogDescription>
-            This is the official syllabus document. You can scroll to view the content.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex-grow p-0">
+      <DialogContent className="h-full w-full max-w-full flex-col p-0 sm:h-[90vh] sm:max-w-3xl">
+        <div className="flex-grow h-full">
           <iframe
             src={pdfUrl}
             className="w-full h-full border-0"
