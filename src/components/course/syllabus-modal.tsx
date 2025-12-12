746e2db8cdc1f674e13ddd11ed11b839194cdde8
Shlok Sane <shloksane11@gmail.com>
2025-12-12 18:46:46 +0000

    make this smaller why it is wide?? Fix it (_for element <DialogHeader>_)

--- src/components/course/syllabus-modal.tsx
+++ src/components/course/syllabus-modal.tsx
@@ -12,7 +12,7 @@ interface SyllabusModalProps {
 export default function SyllabusModal({ isOpen, onClose, pdfUrl }: SyllabusModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="h-full w-full max-w-full flex-col p-0 sm:h-[90vh] sm:max-w-5xl sm:p-0">
+      <DialogContent className="h-full w-full max-w-full flex-col p-0 sm:h-[90vh] sm:max-w-3xl sm:p-0">
         <DialogHeader className="p-4 border-b">
           <DialogTitle>Course Syllabus</DialogTitle>
           <DialogDescription>
