c63108bbb3b7439ba0f97bb1633f4ef48c7c507c
Shlok Sane <shloksane11@gmail.com>
2025-12-12 19:29:17 +0000

    for the resources pop screen i want this button as it was before. (_for

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -44,8 +44,8 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute left-4 top-6 rounded-md p-2 bg-accent ring-offset-background transition-opacity hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
-        <X className="h-5 w-5 text-white" />
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
